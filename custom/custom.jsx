5d2881ba01f71d1ba7995b17f1b47132fb3eec9f
rohit5681 <85388416+rohit5681@users.noreply.github.com>
2023-05-15 00:26:10 +0530

    Create custom.jsx

---
+++ custom/custom.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CustomButton.scss";
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...props }) => (
+  <button
+    className={`${inverted ? "inverted " : " "}${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
+    {...props}
+  >
+    {children}
+  </button>
+);
+export default CustomButton;
